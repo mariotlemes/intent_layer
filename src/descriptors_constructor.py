712a69ca83d84cb0340ab02b9066a7611b8b540e
Mario Teixeira Lemes <mario.lemes@ifg.edu.br>
2023-11-23 15:12:08 -0300

    Update descriptors_constructor.py

--- src/descriptors_constructor.py
+++ src/descriptors_constructor.py
@@ -1,4 +1,4 @@
-import yaml, tarfile, gzip, os, json
+import yaml, tarfile, gzip, os, json, io
 '''
 NFs, throghput, topology
 
@@ -10,131 +10,155 @@ cloud_init_example_VNF/
         cloud_init.cfg
 Reference: https://osm-download.etsi.org/ftp/osm-doc/etsi-nfv-vnfd.html#
 '''
-def create_template_ns():
-    '''TODO: Criar um dicionario de TEMPLATE para os descriptores de serviços da rede (NSd)'''
-    pass
+class DescriptorConstrutor:
+    def create_template_ns():
+        '''TODO: Criar um dicionario de TEMPLATE para os descriptores de serviços da rede (NSd)'''
+        pass
 
-def create_template_vnfd(name_of_intent, number_vfs):
-    '''Receive intent name and number of VFs and generate a JSON file
-    of VNFD'''
-
-    # vnfd (caracteristicas das funções)
-    # DF deployment flavour profile - requisitos de capacidade e performance
-    # VDU (Virtual Deployment Unit)) - representa uma VM individual dentro de uma VNF
-    # ext-cpd (external-connection point descriptor) - especifica caracteristicas de
-    # um ou mais pontos de conexão externos expostos pela função de rede
-
-    # 1o - conteúdo padrão da VNFD em YAML
-    yaml_content = \
-'''vnfd:
-  description: VNFD with 2 CPs to be used on Slice Testing
-  df:
-  - id: default-df
-    instantiation-level:
-    - id: default-instantiation-level
-      vdu-level:
-      - number-of-instances: 1
-        vdu-id: ubuntu_slice-VM
-    vdu-profile:
-    - id: ubuntu_slice-VM
-      min-number-of-instances: 1
-  ext-cpd:
-  - id: eth0-ext
-    int-cpd:
-      cpd: eth0-int
-      vdu-id: ubuntu_slice-VM
-  - id: eth1-ext
-    int-cpd:
-      cpd: eth1-int
-      vdu-id: ubuntu_slice-VM
-  id: slice_basic_vnf
-  mgmt-cp: eth0-ext
-  product-name: slice_basic_vnf
-  provider: OSM
-  sw-image-desc:
-  - id: ubuntu18.04
-    image: ubuntu18.04
-    name: ubuntu18.04
-  - id: ubuntu18.04-aws
-    name: ubuntu18.04-aws
-    image: ubuntu/images/hvm-ssd/ubuntu-artful-17.10-amd64-server-20180509
-    vim-type: aws
-  - id: ubuntu18.04-azure
-    name: ubuntu18.04-azure
-    image: Canonical:UbuntuServer:18.04-LTS:latest
-    vim-type: azure
-  - id: ubuntu18.04-gcp
-    name: ubuntu18.04-gcp
-    image: ubuntu-os-cloud:image-family:ubuntu-1804-lts
-    vim-type: gcp
-  vdu:
-  - cloud-init-file: cloud-config.txt
-    description: ubuntu_slice-VM
-    id: ubuntu_slice-VM
-    int-cpd:
-    - id: eth0-int
-      virtual-network-interface-requirement:
-      - name: eth0
-        virtual-interface:
-          bandwidth: 0
-          type: VIRTIO
-          vpci: 0000:00:0a.0
-    - id: eth1-int
-      virtual-network-interface-requirement:
-      - name: eth1
-        virtual-interface:
-          bandwidth: 0
-          type: VIRTIO
-          vpci: 0000:00:0a.0
-    name: ubuntu_slice-VM
-    sw-image-desc: ubuntu18.04
-    alternative-sw-image-desc:
-    - ubuntu18.04-aws
-    - ubuntu18.04-azure
-    - ubuntu18.04-gcp
-    virtual-compute-desc: ubuntu_slice-VM-compute
-    virtual-storage-desc:
-    - ubuntu_slice-VM-storage
-  version: 1.0
-  virtual-compute-desc:
-  - id: ubuntu_slice-VM-compute
-    virtual-cpu:
-      num-virtual-cpu: 1
-    virtual-memory:
-      size: 1.0
-  virtual-storage-desc:
-  - id: ubuntu_slice-VM-storage
-    size-of-storage: 10'''
+    def create_template_vnfd(name_of_intent, number_vfs):
+        '''Receive intent name and number of VFs and generate a JSON file
+        of VNFD'''
+        json_content = \
+        """vnfd_data =
+            {
+          "vnfd": {
+            "description": "A basic VNF descriptor w/ one VDU",
+            "df": [
+              {
+                "id": "default-df",
+                "instantiation-level": [
+                  {
+                    "id": "default-instantiation-level",
+                    "vdu-level": [
+                      {
+                        "number-of-instances": 1,
+                        "vdu-id": "hackfest_basic-VM"
+                      }
+                    ]
+                  }
+                ],
+                "vdu-profile": [
+                  {
+                    "id": "hackfest_basic-VM",
+                    "min-number-of-instances": 1
+                  }
+                ]
+              }
+            ],
+            "ext-cpd": [
+              {
+                "id": "vnf-cp0-ext",
+                "int-cpd": {
+                  "cpd": "vdu-eth0-int",
+                  "vdu-id": "hackfest_basic-VM"
+                }
+              }
+            ],
+            "id": "teste-VNF-onboarding",
+            "mgmt-cp": "vnf-cp0-ext",
+            "product-name": "mario-teste",
+            "sw-image-desc": [
+              {
+                "id": "ubuntu20.04",
+                "image": "ubuntu20.04",
+                "name": "ubuntu20.04"
+              },
+              {
+                "id": "ubuntu20.04-aws",
+                "name": "ubuntu20.04-aws",
+                "image": "ubuntu/images/hvm-ssd/ubuntu-artful-17.10-amd64-server-20180509",
+                "vim-type": "aws"
+              },
+              {
+                "id": "ubuntu20.04-azure",
+                "name": "ubuntu20.04-azure",
+                "image": "Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest",
+                "vim-type": "azure"
+              },
+              {
+                "id": "ubuntu20.04-gcp",
+                "name": "ubuntu20.04-gcp",
+                "image": "ubuntu-os-cloud:image-family:ubuntu-2004-lts",
+                "vim-type": "gcp"
+              }
+            ],
+            "vdu": [
+              {
+                "id": "hackfest_basic-VM",
+                "name": "hackfest_basic-VM",
+                "sw-image-desc": "ubuntu20.04",
+                "alternative-sw-image-desc": [
+                  "ubuntu20.04-aws",
+                  "ubuntu20.04-azure",
+                  "ubuntu20.04-gcp"
+                ],
+                "virtual-compute-desc": "hackfest_basic-VM-compute",
+                "virtual-storage-desc": [
+                  "hackfest_basic-VM-storage"
+                ],
+                "int-cpd": [
+                  {
+                    "id": "vdu-eth0-int",
+                    "virtual-network-interface-requirement": [
+                      {
+                        "name": "vdu-eth0",
+                        "virtual-interface": {
+                          "type": "PARAVIRT"
+                        }
+                      }
+                    ]
+                  }
+                ]
+              }
+            ],
+            "version": 1,
+            "virtual-compute-desc": [
+              {
+                "id": "hackfest_basic-VM-compute",
+                "virtual-cpu": {
+                  "num-virtual-cpu": 1
+                },
+                "virtual-memory": {
+                  "size": 1
+                }
+              }
+            ],
+            "virtual-storage-desc": [
+              {
+                "id": "hackfest_basic-VM-storage",
+                "size-of-storage": 10
+              }
+            ]
+          }
+        }"""
     # 2o - carregar o conteúdo em YAML para um dicionário PYTHON
-    vnfd_dict = yaml.safe_load(yaml_content)
+        vnfd_dict = json.loads(json_content)
 
-    if 'vnfd' in vnfd_dict and 'df' in vnfd_dict['vnfd']:
-        vnfd_dict['vnfd']['id'] = name_of_intent
-        vnfd_dict['vnfd']['product-name'] = name_of_intent
-        vnfd_dict['vnfd']['df'][0]['instantiation-level'][0]['vdu-level'][0]['number-of-instances'] = 5
+        if 'vnfd' in vnfd_dict and 'df' in vnfd_dict['vnfd']:
+            vnfd_dict['vnfd']['id'] = name_of_intent
+            vnfd_dict['vnfd']['product-name'] = name_of_intent
+            vnfd_dict['vnfd']['df'][0]['instantiation-level'][0]['vdu-level'][0]['number-of-instances'] = 5
 
-    # 3º converter dicionario em yaml
-    yaml_str = yaml.dump(vnfd_dict, default_flow_style=False,
-                         width=float("inf"))
 
-    # remove the last line white line if exists
-    if yaml_str.endswith('\n'):
-        yaml_str = yaml_str[:-1]
+        print(vnfd_dict)
 
-    data = yaml.safe_load(yaml_str)
-
-    data_json = json.dumps(data, indent=2)
-
-    # print(data_json)
-    return data_json
+        # # data = yaml.safe_load(yaml_str)
+        #
+        # # data_json = json.dumps(data, indent=2)
+        #
+        # # print(data_json)
+        # # return data_json
+        # return yaml_str
 
 if __name__ == '__main__':
+    DescriptorConstrutor.create_template_vnfd('teste2', 4)
 
-    create_template_vnfd('slice_basic_vnf', 4)
+# vnfd (caracteristicas das funções)
+# DF deployment flavour profile - requisitos de capacidade e performance
+# VDU (Virtual Deployment Unit)) - representa uma VM individual dentro de uma VNF
+# ext-cpd (external-connection point descriptor) - especifica caracteristicas de
+# um ou mais pontos de conexão externos expostos pela função de rede
 
-    # print(conteudo)
-
-
-
+# 1o - conteúdo padrão da VNFD em YAML
 
 
